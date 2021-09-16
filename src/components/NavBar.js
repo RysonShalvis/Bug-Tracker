e9bbf7f6a170a74cec8b15d1d039e7f87194cd27
RysonShalvis <rysonshalvis@gmail.com>
2021-09-15 21:53:49 -0600

    fix desktop css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,41 +5,44 @@ import dashboardIcon from '../dashboard-icon.png';
 const NavBar = (props) => {
     const { user, logout } = useAuth0();
     return (
-        <div className={`nav-bar ${props.opened}`}>
-            <div className="nav-bar-wrapper">
-                <div className="account">
-                    <img src={user.picture} alt={user.name} className="account-picture"/>
-                    <div className="account-name">{user.name.toUpperCase()}</div>
+        <div className="nav-ctn" >
+            <div className={`nav-bar ${props.opened}`}>
+                <div className="nav-bar-wrapper">
+                    <div className="account">
+                        <img src={user.picture} alt={user.name} className="account-picture"/>
+                        <div className="account-name">{user.name.toUpperCase()}</div>
+                    </div>
                 </div>
-            </div>
-            <div className="underline"></div>
-            <div className="nav-bar-wrapper">
-                <div className="nav-link">
-                    <img src={dashboardIcon} alt="icon" className="nav-icon" />
-                    <div className="nav-name">Dashboard</div>
+                <div className="underline"></div>
+                <div className="nav-bar-wrapper">
+                    <div className="nav-link">
+                        <img src={dashboardIcon} alt="icon" className="nav-icon" />
+                        <div className="nav-name">Dashboard</div>
+                    </div>
+                    <div className="nav-link">
+                        <img src={dashboardIcon} alt="icon" className="nav-icon" />
+                        <div className="nav-name">Dashboard</div>
+                    </div>
+                    <div className="nav-link">
+                        <img src={dashboardIcon} alt="icon" className="nav-icon" />
+                        <div className="nav-name">Dashboard</div>
+                    </div>
+                    <div className="nav-link">
+                        <img src={dashboardIcon} alt="icon" className="nav-icon" />
+                        <div className="nav-name">Dashboard</div>
+                    </div>
+                    <div className="nav-link">
+                        <img src={dashboardIcon} alt="icon" className="nav-icon" />
+                        <div className="nav-name">Dashboard</div>
+                    </div>
+                    <button onClick={() => logout()}>
+                    Logout
+                    </button> 
+                    
                 </div>
-                <div className="nav-link">
-                    <img src={dashboardIcon} alt="icon" className="nav-icon" />
-                    <div className="nav-name">Dashboard</div>
-                </div>
-                <div className="nav-link">
-                    <img src={dashboardIcon} alt="icon" className="nav-icon" />
-                    <div className="nav-name">Dashboard</div>
-                </div>
-                <div className="nav-link">
-                    <img src={dashboardIcon} alt="icon" className="nav-icon" />
-                    <div className="nav-name">Dashboard</div>
-                </div>
-                <div className="nav-link">
-                    <img src={dashboardIcon} alt="icon" className="nav-icon" />
-                    <div className="nav-name">Dashboard</div>
-                </div>
-                <button onClick={() => logout()}>
-                Logout
-            </button> 
-                
             </div>
         </div>
+        
     )
 }
 
