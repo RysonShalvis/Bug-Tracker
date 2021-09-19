a01986dc0ba03939619d863b6b63b799c6360be7
RysonShalvis <rysonshalvis@gmail.com>
2021-09-19 13:35:44 -0600

    finish desktop styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,6 +12,9 @@ const Dashboard = () => {
             <div className="ticket-filter">
                 
             </div>
+            <div className="ticket-filter">
+                
+            </div>
         </div>
     )
 }
